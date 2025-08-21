aa011d41eeb7b7f13c6df86f309af2e4ca8200e3
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-21 10:10:19 -0300

    Conclusão do Ex 3 + Comentários no Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,5 +1,11 @@
 ﻿// 1) Escreva um método recursivo que receba dois inteiros positivos a e n e calcule a elevado a n
 
+
+// 2 elevado a 3 = (2 elevado a 2) * 2 
+// 2 elevado a 2 = (2 elevado a 1) * 2
+// 2 elevado a 1 = 2
+
+
 namespace Ex1
 {
     internal class Program
@@ -7,20 +13,20 @@ namespace Ex1
         static int Potencia(int a, int n)
         {
             int result;
-            if (n == 1)
+            if (n == 1) // caso base
             {
                 result = a;
             }
             else
             {
-                result = Potencia(a, n-1) * a;
+                result = Potencia(a, n-1) * a; // Passo recursivo
             }
             return result;
         }
 
         static void Main(string[] args)
         {
-            int potencia = Potencia(3, 3);
+            int potencia = Potencia(2, 4);
             Console.WriteLine($"Resultado: {potencia}");
         }
     }
---
+++ Ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex3
+{
+    internal class Program
+    {
+        static void Converter(int n)
+        {
+            if (n == 1)
+            {
+                Console.Write(1);
+            }
+            else
+            {
+                Converter(n/2);
+                Console.Write(n%2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Converter(123);
+        }
+    }
+}
