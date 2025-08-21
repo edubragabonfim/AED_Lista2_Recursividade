28464e1ab796aaf64024e6648e144a3ae6d5b93f
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-21 13:49:45 -0300

    Conclusão do Exercício 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex4
+{
+    internal class Program
+    {
+        static int MDC(int x, int y)
+        {
+            int result;
+            if(x == y)
+            {
+                result = x;
+            }
+            else
+            {
+                if(x > y)
+                {
+                    result = MDC(x - y, y);
+                }
+                else
+                {
+                    result = MDC(y, x);
+                }
+            }
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            int resultado = MDC(30, 15);
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+    }
+}
