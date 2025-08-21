025eba449775e603f3b879ac9f7e2b418dff35d0
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-20 22:21:41 -0300

    Conclusão do Exercício 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex2
+{
+    internal class Program
+    {
+        static int SomaIntervalo(int x, int y)
+        {
+            int result;
+            if (x == y)
+            {
+                result = x;
+            }
+            else
+            {
+                result = SomaIntervalo(x, y - 1) + y;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int conta = SomaIntervalo(4, 7);
+            Console.WriteLine($"Resultado: {conta}");
+        }
+    }
+}
