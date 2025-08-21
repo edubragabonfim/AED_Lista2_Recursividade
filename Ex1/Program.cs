3295a2bf3978b8f96fa309bbfa12275cc185e03e
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-20 22:11:04 -0300

    Conclusão do Exercício 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Ex1
+﻿// 1) Escreva um método recursivo que receba dois inteiros positivos a e n e calcule a elevado a n
+
+namespace Ex1
 {
     internal class Program
     {
+        static int Potencia(int a, int n)
+        {
+            int result;
+            if (n == 1)
+            {
+                result = a;
+            }
+            else
+            {
+                result = Potencia(a, n-1) * a;
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int potencia = Potencia(3, 3);
+            Console.WriteLine($"Resultado: {potencia}");
         }
     }
 }
